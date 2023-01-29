1633d9a16a78413db2ccf2d1e70195f6a017da42
eonraider <livewire_voodoo@protonmail.com>
2023-01-29 18:14:14 -0300

    Split tests

--- tests/unit/virustotal/test_virustotal_api.py
+++ tests/unit/virustotal/test_virustotal_api.py
@@ -9,26 +9,29 @@ from reconlib.virustotal.api import VirusTotal
 
 
 class TestVirusTotalAPI:
-    def test_set_api_key(self, api_key, root_dir):
+    def test_set_api_key_from_env(self, api_key, root_dir):
         # Set API key value directly from environment variable
         env_api_key = f"{api_key}_from_env"
         os.environ["VIRUSTOTAL_API_KEY"] = env_api_key
         domain_info = API(target="nmap.org")
         assert domain_info.api_key == env_api_key
+        os.environ.pop("VIRUSTOTAL_API_KEY", None)  # Cleanup environment
 
+    def test_set_api_key_from_init(self, api_key, root_dir):
         # Set API key at instantiation
         inst_api_key = f"{api_key}_from_instance"
         domain_info = API(target="nmap.org", api_key=inst_api_key)
         assert domain_info.api_key == inst_api_key
+        os.environ.pop("VIRUSTOTAL_API_KEY", None)
 
+    def test_set_api_key_from_file(self, api_key, root_dir):
         # Set API key from a file
         api_key_file_path = root_dir.joinpath("tests/unit/virustotal/.test_env")
         with open(api_key_file_path) as file:
             file_api_key = re.search(r"\"(.*)\"", file.read()).group(1)
         domain_info = API(target="nmap.org", api_key=api_key_file_path)
         assert domain_info.api_key == file_api_key
-
-        os.environ.pop("VIRUSTOTAL_API_KEY", None)  # Cleanup environment
+        os.environ.pop("VIRUSTOTAL_API_KEY", None)
 
     def test_undefined_api_key(self):
         with pytest.raises(APIKeyError):
