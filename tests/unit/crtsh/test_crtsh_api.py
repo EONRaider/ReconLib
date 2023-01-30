084e0c8e92868af1876860b246c41c42ef4b641a
eonraider <livewire_voodoo@protonmail.com>
2023-01-30 10:30:16 -0300

    Conform tests to new interface

--- tests/unit/crtsh/test_crtsh_api.py
+++ tests/unit/crtsh/test_crtsh_api.py
@@ -3,27 +3,31 @@ from reconlib import CRTShAPI
 
 class TestCRTShAPI:
     def test_init_crtsh_api(self):
-        crtsh = CRTShAPI(target="test.domain.abc")
+        crtsh = CRTShAPI()
         assert crtsh.wildcard is True
         assert crtsh.include_expired is True
         assert crtsh.encoding == "utf_8"
 
     def test_get_query_url(self):
-        crtsh = CRTShAPI(target="test.domain.abc")
+        crtsh = CRTShAPI()
         assert (
-            crtsh.get_query_url() == "https://crt.sh/?q=%.test.domain.abc&output=json"
+            crtsh.get_query_url(target="test.domain.abc")
+            == "https://crt.sh/?q=%.test.domain.abc&output=json"
         )
 
         crtsh.wildcard = False
-        assert crtsh.get_query_url() == "https://crt.sh/?q=test.domain.abc&output=json"
+        assert (
+            crtsh.get_query_url(target="test.domain.abc")
+            == "https://crt.sh/?q=test.domain.abc&output=json"
+        )
 
         crtsh.include_expired = False
         assert (
-            crtsh.get_query_url()
+            crtsh.get_query_url(target="test.domain.abc")
             == "https://crt.sh/?q=test.domain.abc&output=json&exclude=expired"
         )
 
-    def test_fetch_from_crtsh(
+    def test_fetch_certificates(
         self,
         mocker,
         crtsh_github_response,
@@ -38,7 +42,7 @@ class TestCRTShAPI:
         )
 
         target = "github.com"
-        (domain_info := CRTShAPI(target)).fetch()
+        (domain_info := CRTShAPI()).fetch_certificates(target=target)
 
         assert domain_info.results == parsed_crtsh_github_response
         assert domain_info.subdomains[target] == crtsh_github_domains
--- tests/unit/virustotal/test_virustotal_api.py
+++ tests/unit/virustotal/test_virustotal_api.py
@@ -13,15 +13,13 @@ class TestVirusTotalAPI:
         # Set API key value directly from environment variable
         env_api_key = f"{api_key}_from_env"
         os.environ["VIRUSTOTAL_API_KEY"] = env_api_key
-        domain_info = VirusTotalAPI(target="nmap.org")
-        assert domain_info.api_key == env_api_key
+        assert VirusTotalAPI().api_key == env_api_key
         os.environ.pop("VIRUSTOTAL_API_KEY", None)  # Cleanup environment
 
     def test_set_api_key_from_init(self, api_key, root_dir):
         # Set API key at instantiation
         inst_api_key = f"{api_key}_from_instance"
-        domain_info = VirusTotalAPI(target="nmap.org", api_key=inst_api_key)
-        assert domain_info.api_key == inst_api_key
+        assert VirusTotalAPI(api_key=inst_api_key).api_key == inst_api_key
         os.environ.pop("VIRUSTOTAL_API_KEY", None)
 
     def test_set_api_key_from_file(self, api_key, root_dir):
@@ -29,18 +27,17 @@ class TestVirusTotalAPI:
         api_key_file_path = root_dir.joinpath("tests/unit/virustotal/.test_env")
         with open(api_key_file_path) as file:
             file_api_key = re.search(r"\"(.*)\"", file.read()).group(1)
-        domain_info = VirusTotalAPI(target="nmap.org", api_key=api_key_file_path)
-        assert domain_info.api_key == file_api_key
+        assert VirusTotalAPI(api_key=api_key_file_path).api_key == file_api_key
         os.environ.pop("VIRUSTOTAL_API_KEY", None)
 
     def test_undefined_api_key(self):
         with pytest.raises(APIKeyError):
             # API key not defined neither as an environment variable nor
             # an instance attribute
-            VirusTotalAPI(target="nmap.org")
+            VirusTotalAPI()
 
     def test_headers(self, api_key):
-        domain_info = VirusTotalAPI(target="nmap.org", api_key=api_key)
+        domain_info = VirusTotalAPI(api_key=api_key)
         assert domain_info.headers == {
             "accept": "application/json",
             "x-apikey": api_key,
@@ -49,17 +46,17 @@ class TestVirusTotalAPI:
     def test_get_query_url(self, api_key):
         target = "nmap.org"
         limit = 1000
-        domain_info = VirusTotalAPI(target, api_key=api_key)
+        domain_info = VirusTotalAPI(api_key=api_key)
 
         assert (
             domain_info.get_query_url(
-                endpoint=VirusTotal.SUBDOMAINS, params={"limit": limit}
+                target=target, endpoint=VirusTotal.SUBDOMAINS, params={"limit": limit}
             )
             == f"https://www.virustotal.com/api/v3/domains/{target}/subdomains?"
             f"limit={limit}"
         )
 
-    def test_subdomains(
+    def test_fetch_subdomains(
         self,
         mocker,
         api_key,
@@ -72,8 +69,7 @@ class TestVirusTotalAPI:
             "reconlib.virustotal.api.VirusTotalAPI._query_service",
             return_value=virustotal_subdomains_nmap_response,
         )
-        domain_info = VirusTotalAPI(target="nmap.org", api_key=api_key)
-        assert domain_info.get_subdomains() == virustotal_nmap_subdomains
-        assert domain_info.subdomains == {
-            domain_info.target: virustotal_nmap_subdomains
-        }
+        assert (
+            VirusTotalAPI(api_key=api_key).fetch_subdomains(target="nmap.org")
+            == virustotal_nmap_subdomains
+        )
