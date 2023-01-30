02e0e1623f758f2de4c56e5cec50c62d85630804
eonraider <livewire_voodoo@protonmail.com>
2023-01-30 13:40:25 -0300

    Add copyright and contact notices

--- tests/unit/virustotal/test_virustotal_api.py
+++ tests/unit/virustotal/test_virustotal_api.py
@@ -1,3 +1,28 @@
+"""
+ReconLib: A collection of modules and helpers for active and passive
+reconnaissance of remote hosts.
+
+Author: EONRaider
+GitHub: https://github.com/EONRaider
+Contact: https://www.twitter.com/eon_raider
+
+    Copyright (C) 2023 EONRaider @ keybase.io/eonraider
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see
+    <https://github.com/EONRaider/ReconLib/blob/master/LICENSE>.
+"""
+
 import os
 import re
 
